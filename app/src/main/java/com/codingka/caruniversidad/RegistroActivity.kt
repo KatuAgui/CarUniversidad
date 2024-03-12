3f35bdaedba9ee77fbee4800ffa22939d4143045
Katia Aguilar <katyaguilargarcia@gmail.com>
2024-03-12 09:00:19 -0600

    Diseños de login y registro

---
+++ app/src/main/java/com/codingka/caruniversidad/RegistroActivity.kt
@@ -0,0 +1,24 @@
+package com.codingka.caruniversidad
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Patterns
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import java.util.regex.Pattern
+
+class RegistroActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registro)
+
+    }
+
+}
---
+++ app/src/main/java/com/codingka/caruniversidad/VerificacionCuentaActivity.kt
@@ -0,0 +1,21 @@
+package com.codingka.caruniversidad
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.auth.ktx.userProfileChangeRequest
+import com.google.firebase.ktx.Firebase
+
+class VerificacionCuentaActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_verificacion_cuenta)
+
+    }
+
+}
